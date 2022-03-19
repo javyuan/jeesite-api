0f57cb323a124f349bfbc818a1c26a80ebdb4956
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 19:05:20 +0800

    Delete src directory

--- src/main/java/com/thinkgem/jeesite/common/beanvalidator/DefaultGroup.java
+++
@@ -1,9 +0,0 @@
-package com.thinkgem.jeesite.common.beanvalidator;
-
-/**
- * 默认Bean验证组
- * @author ThinkGem
- */
-public interface DefaultGroup {
-
-}
--- src/main/java/com/thinkgem/jeesite/common/beanvalidator/EditGroup.java
+++
@@ -1,9 +0,0 @@
-package com.thinkgem.jeesite.common.beanvalidator;
-
-/**
- * 编辑Bena验证组
- * @author ThinkGem
- */
-public interface EditGroup {
-
-}
